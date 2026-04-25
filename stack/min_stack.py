ad1f0fd14feda908029a2bd93581f32b2c9c1aa5
Barani132 <barani.b2137@gmail.com>
2026-04-25 21:53:56 +0530

    Add min stack implementation in min_stack.py
    Implement min stack with push, pop, and get_min functions.

---
+++ stack/min_stack.py
@@ -0,0 +1,22 @@
+stack = []
+min_stack = []
+
+def push(x):
+    stack.append(x)
+    if not min_stack or x <= min_stack[-1]:
+        min_stack.append(x)
+
+def pop():
+    if stack[-1] == min_stack[-1]:
+        min_stack.pop()
+    stack.pop()
+
+def get_min():
+    return min_stack[-1]
+
+push(3)
+push(5)
+push(2)
+push(1)
+
+print("Min:", get_min())
