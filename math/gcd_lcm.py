6d86d3f70d5df273a16a165974a3cc64b62500c4
Barani132 <barani.b2137@gmail.com>
2026-04-27 21:12:47 +0530

    Add GCD and LCM functions in gcd_lcm.py
    Implement GCD and LCM functions with example usage.

---
+++ math/gcd_lcm.py
@@ -0,0 +1,12 @@
+# Problem: GCD & LCM
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+
+print("GCD:", gcd(12, 18))
+print("LCM:", lcm(12, 18))
