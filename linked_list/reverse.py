c6da089b60f32c173ae22b866b1e8d9031871434
Barani132 <253927501+Barani132@users.noreply.github.com>
2026-05-02 23:00:16 +0530

    Add reverse_list function to reverse a linked list
    Implement a function to reverse a linked list.

---
+++ linked_list/reverse.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+def reverse_list(head):
+    prev = None
+    curr = head
+
+    while curr:
+        nxt = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nxt
+
+    return prev
+
+# Time: O(n), Space: O(1)
