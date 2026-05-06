076794a9e2a2327d0cfdc4a8fbab54362fb865d2
Barani132 <253927501+Barani132@users.noreply.github.com>
2026-05-06 22:37:35 +0530

    Create string_compression.py

---
+++ string/string_compression.py
@@ -0,0 +1,18 @@
+
+def compress(chars):
+    i = 0
+    res = []
+
+    while i < len(chars):
+        char = chars[i]
+        count = 0
+
+        while i < len(chars) and chars[i] == char:
+            i += 1
+            count += 1
+
+        res.append(char)
+        if count > 1:
+            res.append(str(count))
+
+    return "".join(res)
